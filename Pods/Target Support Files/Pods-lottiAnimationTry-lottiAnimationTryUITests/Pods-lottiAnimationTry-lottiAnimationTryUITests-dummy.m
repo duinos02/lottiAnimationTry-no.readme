4a5c262711968acb3f0b5a078c76d55f3c76da20
duinos02 <yeejeen@naver.com>
2021-02-23 22:37:06 +0900

    new file

---
+++ Pods/Target Support Files/Pods-lottiAnimationTry-lottiAnimationTryUITests/Pods-lottiAnimationTry-lottiAnimationTryUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_lottiAnimationTry_lottiAnimationTryUITests : NSObject
+@end
+@implementation PodsDummy_Pods_lottiAnimationTry_lottiAnimationTryUITests
+@end
---
+++ Pods/Target Support Files/Pods-lottiAnimationTry/Pods-lottiAnimationTry-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_lottiAnimationTry : NSObject
+@end
+@implementation PodsDummy_Pods_lottiAnimationTry
+@end
